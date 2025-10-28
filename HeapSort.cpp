ec4af300cf21ffaaef4c9e4ad731b9fd96c03d3b
Jacob-DeWese <dewese.jn@ufl.edu>
2025-10-28 14:33:21 -0400

    Initial commit

---
+++ HeapSort.cpp
@@ -0,0 +1,17 @@
+//
+// Created by thear on 10/28/2025.
+//
+
+// Min heap to get the lowest rated hotels (average - only one review for each hotel needed)
+
+
+
+// Min heap to get the lowest rated review for a specific hotel (parse through all reviews for a specific hotel name)
+
+
+
+// Max heap to get the highest rated hotels (average - only one review for each hotel needed)
+
+
+
+// Max heap to get the highest rated review for a specific hotel (parse through all reviews for a specific hotel name)
